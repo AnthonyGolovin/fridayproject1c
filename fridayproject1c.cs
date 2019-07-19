0627cc27e949fa474360f4bedeb7d1539b11c00b
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-07-19 08:23:38 -0700

    Add project structure such as bread and pastry class, consolewritelines for bulk ui and content and a PastryStore namespace

---
+++ fridayproject1c.cs
@@ -0,0 +1,23 @@
+    
+using System;
+using System.Collections.Generic;
+using Pastrey.Store;
+
+namespace Pastrey.Store {
+
+  public class Bread
+  {
+    public static void Main()
+    {   
+        Console.WriteLine("Welcome to Pierres Pastries: ");
+        Console.WriteLine("Daily Deal: Bread: (Buy 2 get 1 FREE!) and Pastries: (Buy 1 for $2 or 3 for $5)");
+        Console.WriteLine("================================================================================");
+        Console.WriteLine("Bread Price: $5 per loaf");
+        Console.WriteLine("Pastry Price: $2 per Pastry");
+    }
+  }
+public class Pastry
+{
+
+}
+}
