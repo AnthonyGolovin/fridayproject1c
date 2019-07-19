c05f9f21955713e89cf7411f6abb58f5c9b0da21
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-07-19 11:45:55 -0700

    Had to move things around in order to creat auto implemented properties, so resolving errors and reformatting code to meet objective expectations

--- Store/Pastry.cs
+++ Store/Pastry.cs
@@ -2,37 +2,41 @@ using System;
 
 namespace Pastry.Store
 {
-  class Cart
+  class Bread
   {
-      private int breadCost = 5;
-      private int pastryCost = 2;
+      private int _breadCost { get; set; } 
       private int _breadCart; 
-      private int _pastryCart;
-
-      public Cart(int breadCart, int pastryCart)
+      
+      public Bread(int breadCart, int breadCost)
       {
           _breadCart = breadCart;
-          _pastryCart = pastryCart;
+          _breadCost = 5;
       }
-      public int GetBreadCart()
-      {
-          return _breadCart;
-      }
-      public int GetPastryCart()
-      {
-          return _pastryCart;
-      }
-      public void SetBreadCart(int newBreadCart)
-      {
-          _breadCart = newBreadCart;
-      }
-      public void SetPastryCart(int newPastryCart)
-      {
-          _pastryCart = newPastryCart;
-      }
-       public string GetCart()
-      {
-          return "Bread Total: $" + _breadCart * breadCost + " Pastry Total: $" + _pastryCart * pastryCost;
-      }
+    //    public string GetCart()
+    //   {
+    //       return "Bread Total: $" + _breadCart * breadCost;
+    //   }
   }
+//   class Pastry
+//   {
+//       private int pastryCost = 2;
+//       private int _pastryCart;
+
+//       public Cart(int breadCart, int pastryCart)
+//       {
+//           _breadCart = breadCart;
+//           _pastryCart = pastryCart;
+//       }
+//       public int GetPastryCart()
+//       {
+//           return _pastryCart;
+//       }
+//       public void SetPastryCart(int newPastryCart)
+//       {
+//           _pastryCart = newPastryCart;
+//       }
+//        public string GetCart()
+//       {
+//           return "Bread Total: $" + _breadCart * breadCost + " Pastry Total: $" + _pastryCart * pastryCost;
+//       }
 }
--- fridayproject1c.cs
+++ fridayproject1c.cs
@@ -5,11 +5,11 @@ using Pastry.Store;
 
 namespace Pastry.Store {
 
-  public class Bread
+  public class Store
   {
     public static void Main()
     {   
-      
+        //UI
         Console.WriteLine("Welcome to Pierres Pastries: ");
         Console.WriteLine("Daily Deal: Bread: (Buy 2 get 1 FREE!) and Pastries: (Buy 1 for $2 or 3 for $5)");
         Console.WriteLine("================================================================================");
@@ -20,21 +20,18 @@ namespace Pastry.Store {
         Console.WriteLine("Please input the amount of Pastries you would like: (Number form ex. 1,2,3 ect.)");
         int inputPastry = Convert.ToInt32(Console.ReadLine());
 
-
+        //Menu Items
+        Bread = new Bread("Bread", 5);
 
         Cart finalCart = new Cart(inputBread, inputPastry);
-        // Cart pastry = new Cart(inputPastry);
+        
 
         Console.WriteLine(finalCart);
-        // Console.WriteLine(pastry);
+        
         Console.WriteLine(finalCart.GetBreadCart());
         Console.WriteLine(finalCart.GetPastryCart());
         Console.WriteLine(finalCart.GetCart());
-        // Console.WriteLine(breadCost * inputBread);
+       
     }
   }
-public class Pastry
-{
-
-}
 }
